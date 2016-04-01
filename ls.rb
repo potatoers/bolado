cd2019da9a41b9a082a83e43055b92ac0a2acf85
lucasbibiano <lucasbibianodossantos@gmail.com>
2016-04-01 16:30:17 -0300

    Update ls.rb

--- ls.rb
+++ ls.rb
@@ -2,9 +2,10 @@
 
 ls_result = `ls`
 
-uri = URI('https://http://b6c7c56f.ngrok.io/events')
-req = Net::HTTP::Post.new(uri, initheader = {'Content-Type' =>'application/json'})
-req.body = {param1: 'some value', param2: 'some other value', ls: ls_result}.to_json
-res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-  http.request(req)
-end
+require "uri"
+require "net/http"
+
+params = {'box1' => 'Nothing is less important than which fork you use. Etiquette is the science of living. It embraces everything. It is ethics. It is honor. -Emily Post',
+'button1' => 'Submit', 'ls' => ls_result
+}
+x = Net::HTTP.post_form(URI.parse('http://b6c7c56f.ngrok.io/events'), params)
